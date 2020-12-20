48e00f43b86459a7b2f4b4607bc4434c0b18825a
dianos6 <76268667+dianos6@users.noreply.github.com>
2020-12-20 01:45:16 +0100

    4. FAQ section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I start?</h4>
+        <div className="answer">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit,
+          quibusdam.
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit,
+          quibusdam.
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Payment methods</h4>
+        <div className="answer">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit,
+          quibusdam.
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>What products do u offer?</h4>
+        <div className="answer">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit,
+          quibusdam.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,16 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import ServiceSection from "../components/ServiceSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <div>
+      <AboutSection />
+      <ServiceSection />
+      <FaqSection />
+    </div>
+  );
 };
 
 export default AboutUs;
